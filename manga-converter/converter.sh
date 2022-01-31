e28b535b9db140687b9c7e2a90c36b0ac2ed3858
Kostadin Tonchekliev <kostadintonchekliev@Kostadins-MacBook-Pro.local>
2022-01-31 11:24:33 +0200

    Update on converter script

--- manga-converter/converter.sh
+++ manga-converter/converter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Initial variables
 R='\033[0;31m'
 G='\033[0;32m'
 B='\033[0;34m'
@@ -7,11 +8,22 @@ Y='\033[0;33m'
 LB='\033[1;34m'
 NC='\033[0m'
 
-echo -e "${G}Converter script v2${NC}"
-
+echo -e "${G}Converter script v3${NC}"
 name=()
 i=1
 
+#Main Checker function for making sure that the entered choice is a valid one
+function checker() {
+	while true; do
+		read -n 1 -rep "--: " cur_choice
+		if [[ $(grep -Eic "y|n" <<< "${cur_choice}") == 1 ]]; then
+			export cur_choice
+			break
+		fi
+	done
+}
+
+#Detects existing folders and allows you to select one
 for folder in ~/Documents/HakuNeko/*/
 do
 	name+=("$folder")
@@ -24,6 +36,7 @@ do
 	i=$((i+1))
 done
 
+#Checker if the selected folder is a valid one
 while [[ -z $choice1 ]]
 do
 	read choice1
@@ -36,6 +49,7 @@ done
 
 choice1=$((choice1-1))
 
+#Checks and makes the main Done folder where everthing is compressed
 cd "${name[$choice1]}"
 if [[ $(find . -maxdepth 1 -name 'Done*') == '' ]]
 then
@@ -43,39 +57,22 @@ then
 else 
 	echo -e "${R}Done folder already exists${NC}"
 	echo -e "${LB}Would you like to delete any previous content inside it?${Y} y/n${NC}"
-	while [[ -z $choice ]]
-	do
-		read choice
-		if [[ $choice != "y" && $choice != "n" ]]
-		then
-			choice=''
-			echo -e "${R}Please enter a valid option!${NC}"
-		fi
-	done
-
-	if [[ $choice == "y" ]]
+	checker
+	if [[ "$cur_choice" == "y" ]]
 	then
 		rm -rf Done/*
 		echo "Done folder cleared"
-	fi
-	if [[ $choice == "n" ]]
+	elif [[ "$cur_choice" == "n" ]]
 	then
 		echo "Okay... Pray it doesn't duck shit up"
 	fi
-fi
+	unset cur_choice
+fi	
 
+#Allows you to compress all files into a single one or into individual ones
 echo -e "${LB}Would you like to compress all of them in one file?${Y} y/n${NC}"
-while [[ -z $choice2 ]]
-do
-	read choice2
-	if [[ $choice2 != "y" && $choice2 != "n" ]]
-	then
-		choice2=''
-		echo -e  "${R}Please enter a valid option!${NC}"
-	fi
-done
-
-if [[ $choice2 == "y" ]]
+checker
+if [[ "$cur_choice" == "y" ]]
 then
 	echo -e "${LB}Please enter desired name:${NC}"
 	while [[ -z $com_name ]]
@@ -85,9 +82,7 @@ then
 	echo "Converting started"
 	find . -maxdepth 2 -type f -not -name 'Done*' -not -name '.DS_Store' | sed 's/ /\\ /g' | sort -V | xargs zip -q Done/"$com_name".cbz
 	echo -e "[${G}+${NC}]Done"
-fi
-
-if [[ $choice2 == "n" ]]
+elif [[ "$cur_choice" == "n" ]]
 then
 	for d in */
 	do
@@ -100,28 +95,18 @@ then
 		fi
 	done
 fi
+unset cur_choice
 
+#Allows you to move all of the converted files into the main Manga folder or stays where it is
 echo -e "${LB}Would you like to move the converted data?${Y} y/n${NC}"
-while [[ -z $choice3 ]]
-do
-	read choice3
-	if [[ $choice3 != "y" && $choice3 != "n" ]]
-	then
-		choice3=''
-		echo -e "${R}Please enter a valid choice!${NC}"
-	fi
-done
-
 converted_name=$(echo ${name[$choice1]} | cut -d '/' -f6)
-
-if [[ $choice3 == "y" ]]
+checker
+if [[ "$cur_choice" == "y" ]]
 then
 	mkdir ~/Desktop/Kindle/Manga/"$converted_name"
 	mv "${name[$choice1]}"/Done/* ~/Desktop/Kindle/Manga/"$converted_name"/
 	rm -rf Done
-fi
-
-if [[ $choice3 == "n" ]]
+elif [[ "$cur_choice" == "n" ]]
 then
 	echo "Exiting script without moving data"
 	exit
